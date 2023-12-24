1089959dbf2513714acec0b9638298fbfd56f6f9
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2023-12-24 07:51:53 -0500

    Update patch.sh

--- source/main/patch.sh
+++ source/main/patch.sh
@@ -2,8 +2,11 @@
 
 cd ~
 echo -e "Welcome."
-read -p "Please input the name of the folder containing your CavEX download." $folderName
-read -p "Please choose between [option_1] [option_2] [option_3]. In order for it to work, you MUST type the option EXACTLY." pack
+read -p "Please input the name of the folder containing your CavEX download." folderName
+read -p "
+        Please choose your pack.
+        1. Faithful
+        " pack
 cd $HOME/CavEX-RM/source/packs/$pack
 mv -fv terrain.png $HOME/$folderName/assets/
 mv -fv clouds.png $HOME/$folderName/assets/environment/
