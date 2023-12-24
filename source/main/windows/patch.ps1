f2812b6efba976d14413d6b1761bec4c3afa3a3a
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2023-12-24 07:38:26 -0500

    Delete source/main/windows/patch.ps1

--- source/main/windows/patch.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Welcome."
-$folderName = Read-Host "Please input the name of the folder containing your CavEX download."
-$pack = Read-Host "Please choose between [option_1] [option_2] [option_3]. In order for it to work, you MUST type the option EXACTLY."
-Set-Location -Path "$env:userprofile\CavEX-RM\source\packs\$pack"
-Move-Item -Path "terrain.png" -Destination "$env:userprofile\$folderName\assets\" -Force
-Move-Item -Path "clouds.png" -Destination "$env:userprofile\$folderName\assets\environment\" -Force
-Move-Item -Path "sun.png", "moon.png" -Destination "$env:userprofile\$folderName\assets\environment\" -Force
-Write-Host "Patched successfully."
